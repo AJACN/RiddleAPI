a3f718a19e9e9cdc4c9d0474c8123b1b8aa158d9
ajacn <antonio.jairo.senai@gmail.com>
2025-04-04 10:05:11 -0300

    :rocket: agora vai

--- app.py
+++ app.py
@@ -1,51 +1,114 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import random
+import firebase_admin
+from firebase_admin import credentials, firestore
+from flask_cors import CORS
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
-riddles = [
-    {'id':1, 'riddle':'What do you call a man with no body and no nose?', 'answer':'Nobody nose!'},
+#Pega a variável de ambiente e converte para JSON
+FBKEY = json.loads(os.getenv('CONFIG_FIREBASE'))
 
-    {'id':2, 'riddle':'What do you call a cute door?', 'answer':'Adoorable!'},
+cred = credentials.Certificate(FBKEY)
+firebase_admin.initialize_app(cred)
 
-    {'id':3, 'riddle':'Why did the fly never land on the computer?', 'answer':'It was afraid of the world wide web.'},
+#Conectando com o Firestore da Firebase
+db = firestore.client()
 
-    {'id':4, 'riddle':'How does a bee get to school?', 'answer':'On a buzz!'},
 
-    {'id':5, 'riddle':'What is orange and sounds like a parrot?', 'answer':'A carrot'},
-
-    {'id':6, 'riddle':'What fruit can you never cheer up?', 'answer':'A blueberry.'},
-
-    {'id':7, 'riddle':'What do you call a snail on a ship?', 'answer':'A snailor!'},
-
-    {'id':8, 'riddle':'What type of music do rabbits listen to?', 'answer':'Hip hop!'},
-
-    {'id':9, 'riddle':'What is on the ground and also a hundred feet in the air?', 'answer':'A centipede on its back!'},
-
-    {'id':10, 'riddle':'What kind of room has no doors or windows?', 'answer':'A Mushroom'}
-]
-
+# ---------- ROTA PRINCIPAL DE TESTE ----------
 @app.route('/')
 def index():
     return 'Riddle me this...', 200
 
+
+# ------ MÉTODO GET - CHARADA ALEATÓRIA -------
 @app.route('/riddles', methods=['GET'])
 def riddle():
-    return jsonify(random.choice(riddles))
+    riddles = []
+    list = db.collection('riddles').stream()
 
-@app.route('/riddles/<field>/<search>', methods=['GET'])
-def search(field, search):
-    if field not in ['id']:
-        return jsonify({'message':'ERROR - No results available.'}), 404
+    for item in list:
+        riddles.append(item.to_dict())
 
-    if field == 'id':
-        search = int(search)
-
-    for riddle in riddles:
-        if riddle[field] == search:
-            return jsonify(riddle), 200
+    if riddles:
+        return jsonify(random.choice(riddles)), 200
     else:
-        return jsonify({'mensagem':'ERROR! Riddle not found.'}), 404
+        return jsonify({'message':'Error! No riddles found!'}), 404
+
+
+# -------- MÉTODO GET - CHARADA POR ID --------
+@app.route('/riddles/<id>', methods=['GET'])
+def search(id):
+    doc_ref = db.collection('riddles').document(id)
+    doc = doc_ref.get().to_dict()
+
+    if doc:
+        return jsonify(doc), 200
+    else:
+        return jsonify({'message':'Riddle not found'}), 404
+
+
+# ------- MÉTODO POST - ADICIONAR CHARADA -------
+@app.route('/riddles', methods=['POST'])
+def add_riddle():
+    data = request.json
+
+    if "question" not in data or "answer" not in data:
+        return jsonify({'message':'Error. Question and Answer fields are required'}), 400
+
+    # Counter
+    counter_ref = db.collection('id_control').document('counter')
+    counter_doc = counter_ref.get().to_dict()
+    last_id = counter_doc.get('id')
+    new_id = int(last_id) + 1
+    counter_ref.update({'id': new_id}) #atualização da coleção
+
+    db.collection('riddles').document(str(new_id)).set({
+        "id": new_id,
+        "question": data['question'],
+        "answer": data['answer']
+    })
+
+    return jsonify({'message':'Riddle submitted successfully!'}), 201
+
+# -------- MÉTODO PUT - ALTERAR CHARADA -------
+@app.route('/riddles/<id>', methods=['PUT'])
+def alter_riddle(id):
+    data = request.json
+
+    if "question" not in data or "answer" not in data:
+        return jsonify({'mensagem':'Error. Question and Answer field are required'}), 400
+
+    doc_ref = db.collection('riddles').document(id)
+    doc = doc_ref.get()
+
+    if doc.exists:
+        doc_ref.update({
+            'question': data['question'],
+            'answer': data['answer']
+        })
+        return jsonify({'message':'Riddle updated successfully'}), 201
+    else:
+        return jsonify({'message':'Error. Riddle not found.'}), 404
+
+# ------- MÉTODO DELETE - EXCLUIR CHARADA -------
+@app.route('/riddles/<id>', methods=['DELETE'])
+def delete_riddle(id):
+    doc_ref = db.collection('riddles').document(id)
+    doc = doc_ref.get()
+
+    if not doc.exists:
+        return jsonify({'message':'Error. Riddle not found.'}), 404
+
+    doc_ref.delete()
+    return jsonify({'message':'Riddle deleted successfully!'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
